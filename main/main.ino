260c6f1bc34cbc6a9faf1c2cb6c648e6a29465d6
deweesa <asadeweese@gmail.com>
2024-07-22 20:33:17 -0700

    Now can turn lights on individually

--- main/main.ino
+++ main/main.ino
@@ -18,19 +18,38 @@ void setup()
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  updateShiftRegister();
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Enter LED Number 0 to 7 or 'x' to clear");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  leds = 0;
-  updateShiftRegister();
-  delay(tDelay*3);
-  for(int i = 0; i < 3; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    bitSet(leds, i);
-    updateShiftRegister();
-    delay(tDelay);
-    digitalWrite(LED_BUILTIN, LOW);
+  // leds = 0;
+  // updateShiftRegister();
+  // delay(tDelay*3);
+  // for(int i = 0; i < 3; i++) {
+  //   digitalWrite(LED_BUILTIN, HIGH);
+  //   bitSet(leds, i);
+  //   updateShiftRegister();
+  //   delay(tDelay);
+  //   digitalWrite(LED_BUILTIN, LOW);
+  // }
+
+  if(Serial.available()) {
+    char ch = Serial.read();
+    if(ch >= '0' && ch <='7') {
+      int led = ch-'0';
+      bitSet(leds, led);
+      updateShiftRegister();
+      Serial.print("Turned on LED");
+      Serial.println(led);
+    }
+    if (ch =='x') {
+      leds = 0;
+      updateShiftRegister();
+      Serial.print("Cleared");
+    }
   }
 }
